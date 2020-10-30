8e29aa83b8a250f14e4a6b91504b5a0bacade951
Reem <reemalrashed2@gmail.com>
2020-10-30 17:44:36 +0300

    Added login form

--- login-app/src/App.js
+++ login-app/src/App.js
@@ -1,22 +1,74 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {Button,Form,Container,Row,Col,Card} from 'react-bootstrap';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-    <h1>Login page</h1> 
-    <form>
-      <label >Email</label><br/>
-      <input type="email" placeholder="email" name="email"/><br/>
-      <label >Password</label><br/>
-      <input type="password" placeholder="password" name="password"/><br/>
-      <input type="submit"></input>
-    </form>
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPasswordValid: false,
+      passwordEntered: false
+    }
+  }
+  passwordChangeHandler = (event) => {
+    const password = event.target.value;
+    this.setState({
+      passwordEntered: true
+    });
 
-      </header>
-    </div>
-  );
+    if (password.length < 5 ) {
+      this.setState({
+        isPasswordValid: false
+      });
+    } else {
+      this.setState({
+        isPasswordValid: true
+      });
+    }
+  }
+
+  render() {
+    let passwordBoxStyle = null;
+    if (this.state.passwordEntered && !this.state.isPasswordValid) {
+      passwordBoxStyle = {
+        boxShadow: '0 0 2px 1px rgba(255, 0, 0, 1)',
+      };
+    }
+
+    return (
+      <Container className="Login">
+        <Row>
+          <Col md={{ span: 8, offset: 2 }}>
+            <Card className="login-form-container">
+            <h1> Login</h1>
+
+              <Form>
+                <Form.Group as={Row} controlId="formPlaintextEmail">
+                  <Form.Label column sm="2">
+                    Email
+                  </Form.Label>
+                  <Col>
+                    <Form.Control type="email" placeholder="Enter your email" />
+                  </Col>
+                </Form.Group>
+
+                <Form.Group as={Row} controlId="formPlaintextPassword">
+                  <Form.Label column sm="2">
+                    Password
+                  </Form.Label>
+                  <Col sm="10">
+                    <Form.Control type="password" placeholder="Enter your password" className="password-box" style={passwordBoxStyle} onChange={this.passwordChangeHandler} />
+                  </Col>
+                </Form.Group>
+                <Button variant="primary">Login</Button>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
-export default App;
+export default Login;
